2b3b88de298dc28659a14b646565e9de99008ebd
hanafiah <ibnuyahya@gmail.com>
2012-08-27 16:34:43 +0800

    update comment on sample siri.1.2

--- siri.1.2.php
+++ siri.1.2.php
@@ -3,6 +3,8 @@
 /*
  * Belajar Curl : Siri 1
  * Contoh 2
+ *
+ * Melakukan http get menggunakan set option CURLOPT_URL
  */
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://www.google.com/");
