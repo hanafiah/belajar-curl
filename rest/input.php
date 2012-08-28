c54bb46679cc91e35a27555117fff5e5ac791819
hanafiah <ibnuyahya@gmail.com>
2012-08-28 16:01:25 +0800

    adding rest file

---
+++ rest/input.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Simple REST</title>
+        <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+        <link href="http://twitter.github.com/bootstrap/assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+    </head
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="page-header">
+                    <h1>Simple REST <small>GET, POST, PUT, DELETE</small></h1>
+                </div>
+            </div>
+            <div class="row">
+                <h5>REQUEST_METHOD : <span class="label label-important"><?php echo $_SERVER['REQUEST_METHOD']; ?></label></h5>
+                <hr>
+                <pre class="prettyprint">
+                <legend>Data</legend>
+                    <?php
+                    switch ($_SERVER['REQUEST_METHOD']) {
+                        case 'GET':
+                            $data = $_GET;
+                            break;
+                        case 'POST':
+                            $data = $_POST;
+                            break;
+                        default:
+                            parse_str(file_get_contents('php://input'), $data);
+                            break;
+                    }
+                    print_r($data);
+                    ?>
+                </pre>
+            </div>
+        </div>
+    </body>
+</html>
