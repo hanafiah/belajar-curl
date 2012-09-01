3e13a895d36bccccaa19dac0436e48cc9632690a
hanafiah <ibnuyahya@gmail.com>
2012-09-02 07:59:43 +0800

    change filename

--- siri.3.2.php
+++ siri.3.2.php
@@ -2,23 +2,16 @@
 
 /*
  * Belajar Curl : Siri 3
- * Contoh 2
+ * Contoh 3
  *
- * http post
- * dalam contoh ini kita akan menggunakan option CURLOPT_CUSTOMREQUEST = POST untuk mengaktifkan
- * option POST.
+ * http get
  *
  */
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://lab.ibnuyahya.com/belajar-curl/rest/input.php");
+
+$data = "param1=Value 1&param2=Value 2";
+curl_setopt($ch, CURLOPT_URL, "http://lab.ibnuyahya.com/belajar-curl/rest/input.php?{$data}");
+
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-$data = array(
-    'param1' => 'Value 1',
-    'param2' => 'Value 2'
-);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
 curl_exec($ch);
 curl_close($ch);
